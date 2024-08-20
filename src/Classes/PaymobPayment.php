08cc37ecbe9d417dabb1a676e1a053b536346976
Abdelrahman-Moussa <abdelrahman.moussa@jumia.com>
2024-08-20 12:02:44 +0300

    update paymob integration

--- src/Classes/PaymobPayment.php
+++ src/Classes/PaymobPayment.php
@@ -13,6 +13,7 @@ class PaymobPayment extends BaseController implements PaymentInterface
     private $paymob_api_key;
     private $paymob_integration_id;
     private $paymob_iframe_id;
+    private $paymob_hmac_secret;
 
 
     public function __construct()
@@ -20,6 +21,7 @@ class PaymobPayment extends BaseController implements PaymentInterface
         $this->paymob_api_key = config('nafezly-payments.PAYMOB_API_KEY');
         $this->paymob_integration_id = config('nafezly-payments.PAYMOB_INTEGRATION_ID');
         $this->paymob_iframe_id = config("nafezly-payments.PAYMOB_IFRAME_ID");
+        $this->paymob_hmac_secret = config("nafezly-payments.PAYMOB_HMAC_SECRET");
         $this->currency = config("nafezly-payments.PAYMOB_CURRENCY");
     }
 
@@ -36,29 +38,31 @@ class PaymobPayment extends BaseController implements PaymentInterface
      */
     public function pay($amount = null, $user_id = null, $user_first_name = null, $user_last_name = null, $user_email = null, $user_phone = null, $source = null)
     {
-        $this->setPassedVariablesToGlobal($amount,$user_id,$user_first_name,$user_last_name,$user_email,$user_phone,$source);
+        $this->setPassedVariablesToGlobal($amount, $user_id, $user_first_name, $user_last_name, $user_email, $user_phone, $source);
         $required_fields = ['amount', 'user_first_name', 'user_last_name', 'user_email', 'user_phone'];
         $this->checkRequiredFields($required_fields, 'PayMob');
 
-        $request_new_token = Http::withHeaders(['content-type' => 'application/json'])
-            ->post('https://accept.paymobsolutions.com/api/auth/tokens', [
-                "api_key" => $this->paymob_api_key
-            ])->json();
-
-        $get_order = Http::withHeaders(['content-type' => 'application/json'])
-            ->post('https://accept.paymobsolutions.com/api/ecommerce/orders', [
-                "auth_token" => $request_new_token['token'],
-                "delivery_needed" => "false",
+        $intention_request = Http::withHeaders(['content-type' => 'application/json'])
+            ->post('https://accept.paymob.com/api/acceptance/intention/create', [
+                "api_key" => $this->paymob_api_key,
                 "amount_cents" => $this->amount * 100,
-                "items" => []
-            ])->json();
-
-        $get_url_token = Http::withHeaders(['content-type' => 'application/json'])
-            ->post('https://accept.paymobsolutions.com/api/acceptance/payment_keys', [
-                "auth_token" => $request_new_token['token'],
-                "expiration" => 36000,
-                "amount_cents" => $get_order['amount_cents'],
-                "order_id" => $get_order['id'],
+                "currency" => $this->currency,
+                "delivery_needed" => "false",
+                "items" => [],
+                "shipping_data" => [
+                    "apartment" => "NA",
+                    "email" => $this->user_email,
+                    "floor" => "NA",
+                    "first_name" => $this->user_first_name,
+                    "street" => "NA",
+                    "building" => "NA",
+                    "phone_number" => $this->user_phone,
+                    "postal_code" => "NA",
+                    "city" => "NA",
+                    "country" => "NA",
+                    "last_name" => $this->user_last_name,
+                    "state" => "NA"
+                ],
                 "billing_data" => [
                     "apartment" => "NA",
                     "email" => $this->user_email,
@@ -67,21 +71,49 @@ class PaymobPayment extends BaseController implements PaymentInterface
                     "street" => "NA",
                     "building" => "NA",
                     "phone_number" => $this->user_phone,
-                    "shipping_method" => "NA",
                     "postal_code" => "NA",
                     "city" => "NA",
                     "country" => "NA",
                     "last_name" => $this->user_last_name,
                     "state" => "NA"
-                ],
-                "currency" => $this->currency,
-                "integration_id" => $this->paymob_integration_id
+                ]
             ])->json();
 
+        if (!isset($intention_request['id'])) {
+            throw new \Exception('Failed to create payment intention');
+        }
+
+        $payment_request = Http::withHeaders(['content-type' => 'application/json'])
+            ->post('https://accept.paymob.com/api/acceptance/payment_keys', [
+                "auth_token" => $intention_request['token'],
+                "amount_cents" => $this->amount * 100,
+                "currency" => $this->currency,
+                "integration_id" => $this->paymob_integration_id,
+                "order_id" => $intention_request['id'],
+                "billing_data" => [
+                    "apartment" => "NA",
+                    "email" => $this->user_email,
+                    "floor" => "NA",
+                    "first_name" => $this->user_first_name,
+                    "street" => "NA",
+                    "building" => "NA",
+                    "phone_number" => $this->user_phone,
+                    "postal_code" => "NA",
+                    "city" => "NA",
+                    "country" => "NA",
+                    "last_name" => $this->user_last_name,
+                    "state" => "NA"
+                ]
+            ])->json();
+
+        if (!isset($payment_request['token'])) {
+            throw new \Exception('Failed to create payment key');
+        }
+
         return [
-            'payment_id'=>$get_order['id'],
+            'payment_id' => $intention_request['id'],
             'html' => "",
-            'redirect_url'=>"https://accept.paymobsolutions.com/api/acceptance/iframes/" . $this->paymob_iframe_id . "?payment_token=" . $get_url_token['token']
+            'redirect_url' => "https://accept.paymobsolutions.com/api/acceptance/iframes/" . $this->paymob_iframe_id . "?payment_token=" . $payment_request['token']
         ];
     }
 
@@ -91,31 +123,33 @@ class PaymobPayment extends BaseController implements PaymentInterface
      */
     public function verify(Request $request): array
     {
-        $string = $request['amount_cents'] . $request['created_at'] . $request['currency'] . $request['error_occured'] . $request['has_parent_transaction'] . $request['id'] . $request['integration_id'] . $request['is_3d_secure'] . $request['is_auth'] . $request['is_capture'] . $request['is_refunded'] . $request['is_standalone_payment'] . $request['is_voided'] . $request['order'] . $request['owner'] . $request['pending'] . $request['source_data_pan'] . $request['source_data_sub_type'] . $request['source_data_type'] . $request['success'];
+        $hmac = $request->header('Hmac');
+        $data = $request->all();
 
-        if ( hash_equals(hash_hmac('sha512', $string, config('nafezly-payments.PAYMOB_HMAC')),$request['hmac']) ){
-            if ($request['success'] == "true") {
+        $calculatedHmac = hash_hmac('sha512', json_encode($data), $this->paymob_hmac_secret);
+
+        if (hash_equals($calculatedHmac, $hmac)) {
+            if ($data['success'] == true) {
                 return [
                     'success' => true,
-                    'payment_id'=>$request['order'],
+                    'payment_id' => $data['order']['id'],
                     'message' => __('nafezly::messages.PAYMENT_DONE'),
-                    'process_data' => $request->all()
+                    'process_data' => $data
                 ];
             } else {
                 return [
                     'success' => false,
-                    'payment_id'=>$request['order'],
-                    'message' => __('nafezly::messages.PAYMENT_FAILED_WITH_CODE',['CODE'=>$this->getErrorMessage($request['txn_response_code'])]),
-                    'process_data' => $request->all()
+                    'payment_id' => $data['order']['id'],
+                    'message' => __('nafezly::messages.PAYMENT_FAILED_WITH_CODE', ['CODE' => $this->getErrorMessage($data['data']['txn_response_code'])]),
+                    'process_data' => $data
                 ];
             }
-
         } else {
             return [
                 'success' => false,
-                'payment_id'=>$request['order'],
+                'payment_id' => $data['order']['id'] ?? null,
                 'message' => __('nafezly::messages.PAYMENT_FAILED'),
-                'process_data' => $request->all()
+                'process_data' => $data
             ];
         }
     }
@@ -146,7 +180,6 @@ class PaymobPayment extends BaseController implements PaymentInterface
         $refund_process = Http::withHeaders(['content-type' => 'application/json'])
             ->post('https://accept.paymob.com/api/acceptance/void_refund/refund',['auth_token'=>$request_new_token['token'],'transaction_id'=>$transaction_id,'amount_cents'=>$amount])->json();
 
-        dd($refund_process);
         return [
             'transaction_id'=>$transaction_id,
             'amount'=>$amount,
